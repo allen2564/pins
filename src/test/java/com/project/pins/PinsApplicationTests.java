8f9b020a0ace5b6e92f519380312dd890d5757a4
allen2564 <97856988+allen2564@users.noreply.github.com>
2023-10-09 14:55:48 -0500

    full app

---
+++ src/test/java/com/project/pins/PinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
