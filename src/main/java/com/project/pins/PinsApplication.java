ff07c46181fc9aa324bd7568ebf7156bac6285fb
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2023-11-01 12:35:26 -0500

    update app

--- src/main/java/com/project/pins/PinsApplication.java
+++ src/main/java/com/project/pins/PinsApplication.java
@@ -1,5 +1,7 @@
 package com.project.pins;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PinsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PinsApplication.class, args);
+	/* 	SpringApplication.run(PinsApplication.class, args); */
+		SpringApplication app = new SpringApplication(PinsApplication.class);
+		String port = System.getenv("PORT");
+		app.setDefaultProperties(Collections.singletonMap("server.port", port == null ? "8080" : port));
+		app.run(args);
 	}
 
 }
